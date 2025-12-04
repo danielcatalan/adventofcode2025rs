8cdf138e374f3b8e075467068a5c3876d1dda22f
Daniel Catalan <dancatalan415@gmail.com>
2025-12-04 01:01:06 -0800

    start day4

---
+++ day4/build.rs
@@ -0,0 +1,46 @@
+use serde::Deserialize;
+use std::{
+    fs::{self, File},
+    io::Write,
+};
+use ureq;
+
+#[derive(Deserialize)]
+struct CookieSettings {
+    content: String,
+}
+
+fn main() {
+    const INPUT_FILE: &str = "input/input.txt";
+
+    // Check if input already exists
+    {
+        let f = File::open(INPUT_FILE);
+        if let Ok(_file) = f {
+            return; // file already exists
+        }
+    }
+
+    let year = 2025;
+    let day = 4;
+    let url_day = format!("https://adventofcode.com/{}/day/{}/input", year, day);
+
+    // Read Cookies
+    let s = fs::read_to_string("../cookie.toml").unwrap();
+    let cookie: CookieSettings = toml::from_str(&s).unwrap();
+
+    // Get input though URL
+    let header_value = format!("session={}; Domain=.adventofcode.com", cookie.content);
+
+    let body = ureq::get(url_day.as_str())
+        .header("Cookie", header_value.as_str())
+        .call()
+        .unwrap()
+        .body_mut()
+        .read_to_string()
+        .unwrap();
+
+    let mut f = File::create(INPUT_FILE).unwrap();
+
+    f.write_all(body.as_bytes()).unwrap();
+}
---
+++ day4/src/bin/part1.rs
@@ -0,0 +1,18 @@
+use day4::solution;
+
+use std::{fs::File, io::BufReader};
+
+
+
+fn main() {
+
+    println!("Calculating solution1...\n");
+    let f = File::open("day4/input/input.txt").unwrap();
+
+    let reader = BufReader::new(f);
+    let solution = solution::solve_solution1(reader);
+    println!("Solution1: {solution}");
+
+    println!("\n...Done.")
+}
+
