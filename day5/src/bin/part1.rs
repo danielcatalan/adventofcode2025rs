2d281fd6e961d3c08cce1df0935ca8e266a2f47c
Daniel Catalan <dancatalan415@gmail.com>
2025-12-08 00:21:14 -0800

    init day5

---
+++ day5/src/bin/part1.rs
@@ -0,0 +1,18 @@
+use day5::solution;
+
+use std::{fs::File, io::BufReader};
+
+
+
+fn main() {
+
+    println!("Calculating solution1...\n");
+    let f = File::open("day5/input/input.txt").unwrap();
+
+    let reader = BufReader::new(f);
+    let solution = solution::solve_solution1(reader);
+    println!("Solution1: {solution}");
+
+    println!("\n...Done.")
+}
+
