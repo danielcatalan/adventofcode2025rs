5216aa51dba599229244dfdc53a4afd3c6a4c5d1
Daniel Catalan <dancatalan415@gmail.com>
2025-12-11 15:12:30 -0800

    start day8

---
+++ day8/src/bin/day8part1.rs
@@ -0,0 +1,18 @@
+use day8::solution;
+
+use std::{fs::File, io::BufReader};
+
+
+
+fn main() {
+
+    println!("Calculating solution1...\n");
+    let f = File::open("day8/input/input.txt").unwrap();
+
+    let reader = BufReader::new(f);
+    let solution = solution::solve_solution1(reader);
+    println!("Solution1: {solution}");
+
+    println!("\n...Done.")
+}
+
---
+++ day8/src/bin/day8part2.rs
@@ -0,0 +1,17 @@
+use day8::solution;
+
+use std::{fs::File, io::BufReader};
+
+
+
+fn main() {
+
+    println!("Calculating solution2...\n");
+    let f = File::open("day8/input/input.txt").unwrap();
+
+    let reader = BufReader::new(f);
+    let solution = solution::solve_solution2(reader);
+    println!("Solution2: {solution}");
+
+    println!("\n...Done.")
+}
