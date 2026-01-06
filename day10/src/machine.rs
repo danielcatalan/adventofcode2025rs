2035434da4f25596ac920c393c93f29d507fdebd
Daniel Catalan <dancatalan415@gmail.com>
2026-01-06 02:20:15 -0800

    parsing working

--- day10/src/machine.rs
+++ day10/src/machine.rs
@@ -1,1 +1,13 @@
-pub struct Machine;
+pub struct Machine {
+    final_led_state_mask: usize,
+    button_wiring_masks: Vec<usize>,
+}
+
+impl Machine {
+    pub fn new(led_mask: usize, wiring_mask: Vec<usize>) -> Self {
+        Self {
+            final_led_state_mask: led_mask,
+            button_wiring_masks: wiring_mask,
+        }
+    }
+}
--- day10/src/parse.rs
+++ day10/src/parse.rs
@@ -1,13 +1,12 @@
+use regex::Regex;
 use std::io::BufRead;
 use std::sync::LazyLock;
-use regex::Regex;
 
 use crate::{factory::Factory, machine::Machine};
 
-
-
 pub fn parse_factory<R: BufRead>(reader: R) -> Factory {
-    let machines = reader.lines()
+    let machines = reader
+        .lines()
         .map(|line| line.unwrap())
         .map(|line| parse_machine(&line))
         .collect();
@@ -22,6 +21,71 @@ pub fn parse_factory<R: BufRead>(reader: R) -> Factory {
  *
  */
 
-fn parse_machine(line: &str) -> Machine{
-    todo!()
+fn parse_machine(line: &str) -> Machine {
+    let led_mask = parse_leds(line);
+    let button_wirings = parse_buttons(line);
+    Machine::new(led_mask, button_wirings)
+}
+
+fn parse_leds(line: &str) -> usize {
+    static RE_LEDS: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"\[(?P<leds>[#.]{1,})\]").unwrap());
+    let caps = RE_LEDS
+        .captures(line)
+        .expect("Expected Led mask during regex parse");
+    let leds = caps["leds"].as_bytes();
+    let mut led_mask = 0;
+    for (i, b) in leds.iter().enumerate() {
+        if *b == b'#' {
+            led_mask |= 1 << i;
+        }
+    }
+    led_mask
+}
+
+fn parse_buttons(line: &str) -> Vec<usize> {
+    static RE_BUTTONS: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"\((?P<button>[0-9,]{1,})\)").unwrap());
+    let button_wirings = RE_BUTTONS
+        .captures_iter(line)
+        .map(|cap| cap["button"].to_owned())
+        .map(|button| parse_wiring(&button))
+        .collect();
+    button_wirings
+}
+
+fn parse_wiring(s: &str) -> usize {
+    let wiring = s
+        .split(',')
+        .map(|s| s.parse::<usize>().unwrap())
+        .map(|b| 1 << b)
+        .fold(0, |a, b| a | b);
+    wiring
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_leds() {
+        let line = "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}";
+        let led_mask = parse_leds(line);
+        assert_eq!(8, led_mask);
+
+        let line = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}";
+        let led_mask = parse_leds(line);
+        assert_eq!(6, led_mask)
+    }
+
+    #[test]
+    fn test_parse_button_wiring() {
+        let line = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}";
+        let button_wiring_masks = parse_buttons(line);
+        assert_eq!(6, button_wiring_masks.len());
+        assert_eq!(8, button_wiring_masks[0]); // (3)
+        assert_eq!(10, button_wiring_masks[1]); // (1,3)
+        assert_eq!(4, button_wiring_masks[2]); // (2)
+        assert_eq!(3, button_wiring_masks[5]); // (0,1)
+    }
 }
